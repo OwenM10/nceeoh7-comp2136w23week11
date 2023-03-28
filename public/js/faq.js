87839d40003c6108cf287d61a18c813cb2d78eb4
OwenM10 <124073767+OwenM10@users.noreply.github.com>
2023-03-28 17:50:18 +0000

    end of week 11 code

--- public/js/faq.js
+++ public/js/faq.js
@@ -2,6 +2,54 @@
 
 const $ = (selector) => document.querySelector(selector);
 
+let timerCounter = 10;
+let timer;
+
+
+const goToTerms= ()=>{
+    timerCounter -= 1;
+
+    if(timerCounter>0){
+        $("#seconds").textContent = timerCounter;
+    }else{
+        window.location.href = "terms"
+    }
+}
+
+const acceptTerms = ()=>{
+    clearInterval(timer);
+    $("#terms").setAttribute("class", "hidden");
+}
+
+const toggleQuestion = (evt)=>{
+    evt.currentTarget.classList.toggle("minus");
+    let answerDiv = evt.currentTarget.nextElementSibling;
+
+    answerDiv.classList.toggle("open");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
+    $("#seconds").textContent = timerCounter;
+    //step 1: check if user accepted terms
+    const query = window.location.search;
+    const urlParameters = new URLSearchParams(query);
+    const accepted = urlParameters.get("accepted");
+    //console.log(urlParameters.get("first_name")+ "blah blah");
+
+    
+
+    $("#accept").addEventListener("click", acceptTerms);
+
+    const h2Elements = document.querySelectorAll("h2");
+
+    for(let h2 of h2Elements){
+        h2.addEventListener("click",toggleQuestion);
+    }
+
+    if (accepted){
+        $("#terms").setAttribute("class", "hidden");
+    }else{
+        timer = setInterval(goToTerms, 1000);
+    }
 });
